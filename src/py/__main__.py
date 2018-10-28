e00d9f59e93ce08bc6763b1f2fba1efb5c26dd7f
Jason Ye <jasony37@gmail.com>
2018-10-28 14:10:05 -0400

    Separate arg parsing into separate file

--- src/py/__main__.py
+++ src/py/__main__.py
@@ -1,12 +1,19 @@
 import os
+import argparse
 import vis.core
 import proc.readdata
 
 
+def args_setup():
+    args_parser = argparse.ArgumentParser()
+    args_parser.add_argument('datadir', help="Path to cabspottingdata")
+    return args_parser.parse_args()
+
+
 def main():
-    cabs_file = r"C:\Users\Jason\Desktop\BDownloads\cabspottingdata"
-    cab_traces_file = os.path.join(cabs_file, "cab_traces.pickle")
-    cab_reader = proc.readdata.CabReader(cabs_file)
+    args = args_setup()
+    cab_traces_file = os.path.join(args.datadir, "cab_traces.pickle")
+    cab_reader = proc.readdata.CabReader(args.datadir)
     cab_reader.read_cablist()
     if os.path.isfile(cab_traces_file):
         cab_reader.cabtraces_from_save(cab_traces_file)
